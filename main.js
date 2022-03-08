ec3684c0ba7e8a4084fe1fa970c0ecab40cc3f41
Stephen Korzenstein <skorzenstein@gmail.com>
2022-03-08 14:58:46 -0500

    updated js and added header css

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 let checkbox = document.getElementById('openSidebarMenu')
-let buttonX = document.getElementById('buttonX')
-
-console.log(checkbox);
-console.log(buttonX);
 
 function hideNav() {
-    checkbox.value.toggleClass('false');
+    checkbox.checked = false;
 }
